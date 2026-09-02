0e7c821e39b4169c6c5880f0fa107e5b9046ab32
Bart12400 <robi.dzialak@gmail.com>
2026-09-02 11:36:19 +0200

    Disable AarMetadata globally and strip extra AndroidX dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,13 +24,6 @@ android {
     }
 }
 
-tasks.withType(com.android.build.gradle.internal.tasks.AarMetadataTask::class.java).configureEach {
-    enabled = false
-}
-
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
     implementation("com.arthenica:ffmpeg-kit-full:4.5.LTS")
 }
